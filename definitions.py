077ef3a53140909c85a7b7e0a4e8dfe33b925c8f
Adela Jaworowska <adela.jaworowska@gmail.com>
2020-11-09 21:39:39 +0100

    Initial commit

---
+++ definitions.py
@@ -0,0 +1,15 @@
+# SNR project
+# Definitions
+
+dataset_name = 'jerzydziewierz/bee-vs-wasp/activity'
+raw_data_dir = 'raw_data'
+output_datasets_dir = 'datasets'
+
+train_images_file = output_datasets_dir + '/' + 'train_images.tfrec'
+test_images_file  = output_datasets_dir + '/' +  'test_images.tfrec'
+val_images_file   = output_datasets_dir + '/' +   'val_images.tfrec'
+
+train_labels_file = output_datasets_dir + '/' + 'train_labels.tfrec'
+test_labels_file  = output_datasets_dir + '/' +  'test_labels.tfrec'
+val_labels_file   = output_datasets_dir + '/' +   'val_labels.tfrec'
+
